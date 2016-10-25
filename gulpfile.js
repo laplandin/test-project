3c206588dd3b518dc2834c7a719c76d00e7b7c4d
laplandin <deniskakhv@gmail.com>
2016-10-25 21:01:00 +0300

    workflow: gulp, less, prefixer, style, serve

---
+++ gulpfile.js
@@ -0,0 +1,54 @@
+"use strict";
+
+var gulp = require("gulp");
+var less = require("gulp-less");
+var plumber = require("gulp-plumber");
+var postcss = require("gulp-postcss");
+var autoprefixer = require("autoprefixer");
+var server = require("browser-sync").create();
+var csso = require("gulp-csso");
+var del = require("del");
+var rename = require("gulp-rename");
+
+gulp.task("copy", function() {
+  gulp.src([
+    "img/**/*{.jpg,.png,.svg}",
+    "js/**/*.js",
+    "fonts/**/*{.woff,.woff2}",
+    "*.html"
+  ], {
+    base: "."
+  })
+  .pipe(gulp.dest("build"));
+});
+
+gulp.task("copyHtml", function() {
+  gulp.src("*.html")
+  .pipe(gulp.dest("build"));
+});
+
+gulp.task("style", function() {
+  gulp.src("less/style.less")
+    .pipe(plumber())
+    .pipe(less())
+    .pipe(postcss([
+      autoprefixer({browsers: [
+        "last 3 version"
+      ]})
+    ]))
+    .pipe(gulp.dest("css"))
+    .pipe(server.stream());
+});
+
+gulp.task("serve", ["style"], function() {
+  server.init({
+    server: ".",
+    notify: false,
+    open: true,
+    ui: false
+  });
+
+  gulp.watch("less/**/*.less", ["style"]);
+  gulp.watch("*.html", ["copyHtml"]);
+  gulp.watch("build/*.html").on("change", server.reload);
+});
